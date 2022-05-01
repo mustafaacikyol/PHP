a639d78802b441ccd52d2ffca78b3533285fd0bf
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-05-01 14:56:57 +0300

    Create 01-echo-print.php

---
+++ 01-echo-print.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>
+		<?php
+			echo "Echo ve Print Kullanımı"
+		?>
+	</title>
+</head>
+
+<body>
+	<p>Mustafa AÇIKYOL</p>
+
+	<?php
+		echo "Mustafa AÇIKYOL";
+		echo "<br>";
+		print "Php İle Programlama";
+	?>
+
+</body>
+
+</html>
