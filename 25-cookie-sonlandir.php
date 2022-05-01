4a157e2391a7edd0301e906106da112aa8535f73
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-05-01 15:39:12 +0300

    Create 25-cookie-sonlandir.php

---
+++ 25-cookie-sonlandir.php
@@ -0,0 +1,20 @@
+<?php
+
+    setcookie("yonetici", "");
+    //setcookie("yonetici","Mustafa Acikyol",time()-10*60);
+    header("location:25-cookie-oturum-acik-1.php");
+
+?>
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Cookie Sonlandır</title>
+</head>
+
+<body>
+</body>
+
+</html>
