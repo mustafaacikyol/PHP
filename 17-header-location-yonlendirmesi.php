1864db634f5d276196c77c21018761f8b3f22b20
Mustafa Açıkyol <95076715+mustafaacikyol@users.noreply.github.com>
2022-05-01 15:26:02 +0300

    Create 17-header-location-yonlendirmesi.php

---
+++ 17-header-location-yonlendirmesi.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Header ile Location Yönlendirmesi</title>
+</head>
+
+<body>
+    <?php
+        // Php de bir saydafan başka bir sayfaya direkt, doğrudan yönlendirme yapılacaksa header("location: sayfa ve adres yazılır");
+
+        header("location:23-upload.php")
+
+    ?>
+</body>
+
+</html>
